5abc60d220e9570f7e4f70662fc993efc3e9b89c
Carlos Antonio Sales de Oliveira Junior <54126980+jrcarlosanto@users.noreply.github.com>
2022-03-31 19:53:07 -0300

    v3

--- src/routes/calcular.router.js
+++ src/routes/calcular.router.js
@@ -33,8 +33,4 @@ calcularRoute.post('/calculadora', (req, res) => {
     }
 });
 
-calcularRoute.get('', (req, res) => {
-    res.send(`Consulta inválida. Essa é um exemplo de consulta válida http://localhost:3000/soma?a=1&b=1`);
-});
-
 module.exports=calcularRoute;
